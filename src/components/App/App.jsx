6b6ccd846d286b6b21ebee6a523730980003ecc5
NewHope <rimondtt@gmail.com>
2022-05-13 20:04:21 +0300

    remaked-with-hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,83 +1,92 @@
-import React, {Component} from 'react';
-import { nanoid } from 'nanoid'
+import { useState, useEffect, useRef } from 'react';
+import { nanoid } from 'nanoid';
 
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
 import { Container, ContactsSection } from './App.styled';
 
-export class App extends Component {
+export const App = () => {
+  // state = {
+  //   contacts: [  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+  //   {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+  //   {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+  //   {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}],
+  //   filter: '',
+  // }
 
-  state = {
-    contacts: [  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}],
-    filter: '',
-  }
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
 
-  onFormSubmit = (values, actions) => {
+  const [filter, setFilter] = useState('');
+  const isFirstRender = useRef(true);
 
-    const isDublicate = this.state.contacts.find(contact => {
-      return contact.name === values.name
-    })
+  const onFormSubmit = (values, actions) => {
+    const isDublicate = contacts.find(contact => {
+      return contact.name === values.name;
+    });
 
-    if(isDublicate){
+    if (isDublicate) {
       alert(`${values.name} is already in contacts`);
-      return 
+      return;
     }
 
-    this.setState(prevState => ({
-      contacts:[
-        {name: values.name,
-         number: values.number,
-         id: nanoid()},
-         ...prevState.contacts]
-    }))
+    setContacts(contacts => [
+      {
+        name: values.name,
+        number: values.number,
+        id: nanoid(),
+      },
+      ...contacts,
+    ]);
+
     actions.resetForm();
-  }
+  };
 
-  onFilterChange = (e) => {
-    this.setState({filter: e.target.value})
-  }
+  const onFilterChange = e => {
+    setFilter(e.target.value);
+  };
 
-  onDeleteBtbClick = (id) => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => !(contact.id === id))
-    }))
-  }
+  const onDeleteBtnClick = id => {
+    setContacts(contacts => contacts.filter(contact => !(contact.id === id)));
+  };
 
-  componentDidUpdate (_, prevState) {
-
-    if(prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
     }
-  }
 
-  componentDidMount () {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  useEffect(() => {
     const savedContacts = JSON.parse(localStorage.getItem('contacts'));
-    if (savedContacts !== null) {
-      this.setState({
-        contacts: savedContacts
-      })
-    }
-  }
+    savedContacts && setContacts(savedContacts);
+  }, []);
 
-  render() {   
-    const {contacts, filter} = this.state; 
-    const normalizedFilter = filter.toLowerCase();
-    const filteredContacts = contacts.filter((contact) => contact.name.toLowerCase().includes(normalizedFilter))
+  const normalizedFilter = filter.toLowerCase();
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter)
+  );
 
-    return(
+  return (
     <Container>
       <h1>Phonebook</h1>
-      <ContactForm onFormSubmit={this.onFormSubmit}></ContactForm>
+      <ContactForm onFormSubmit={onFormSubmit}></ContactForm>
 
       <h2>Contacts</h2>
       <ContactsSection>
-        <Filter onFilterChange={this.onFilterChange} value={filter}></Filter>
-        <ContactList filteredContacts={filteredContacts} onDeleteBtbClick={this.onDeleteBtbClick}></ContactList>
+        <Filter onFilterChange={onFilterChange} value={filter}></Filter>
+        <ContactList
+          filteredContacts={filteredContacts}
+          onDeleteBtbClick={onDeleteBtnClick}
+        ></ContactList>
       </ContactsSection>
     </Container>
-  )};
+  );
 };
